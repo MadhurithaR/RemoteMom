6546a109f9148def394eac1cfe7eaa5c558dbffb
madhu <madhu@DESKTOP-FG8FL5O>
2018-04-14 12:38:33 -0700

    create classes and properties

---
+++ RemoteMom/Account.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoteMom
+{
+    class Account
+    {
+        public string AccountName { get; set; }
+        public string classes { get; set; }
+        public string Homework { get; set; }
+        public DateTime timeAllocated { get; set; }
+        public string Resources { get; set; }
+        public string daysofWeek { get; set; }
+    }
+}
---
+++ RemoteMom/HomeWork.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoteMom
+{
+    class HomeWork
+    {
+        public string HomeworkName { get; set; }
+        public DateTime TimeAllocated { get; set; }
+        public DateTime SubmissionDateTime { get; set; }
+        public DateTime DayofCompletion { get; set; }
+        public string Resources { get; set; }
+        public string AssociatedClass { get; set; }
+        public DateTime AssociatedClassDateTime { get; set; }
+    }
+}
